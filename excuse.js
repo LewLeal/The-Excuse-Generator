470e96062f9b3a806a656c9379367225ba00554d
Lewis Leal <lewisleal@genesnextstep.com>
2021-08-16 13:47:32 -0400

    Excuse generator inicial

---
+++ excuse.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+};
+let randomWhoPosition = Math.floor(Math.random()* who.length);
