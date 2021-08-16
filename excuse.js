63f2d9095cd18c518423147f64500660897c9bc9
lewleal <lewleal2012@gmail.com>
2021-08-16 15:41:41 -0400

    Excuse generator ok

--- excuse.js
+++ excuse.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   let who = ["the dog", "my granma", "his turtle", "my bird"];
   let what = ["eat", "pissed", "crushed", "broked"];
-  let what = ["eat", "pissed", "crushed", "broked"];
+  let actions = ["excuse", "Dog", "Crazy", "LOL"];
   let when = [
     "before the class",
     "right in time",
@@ -9,5 +9,15 @@ window.onload = function () {
     "during my lunch",
     "while I was praying",
   ];
+  let randomWhoPosition = Math.floor(Math.random()* who.length);
+  let randomWhatPosition = Math.floor(Math.random()* what.length);
+  let randomWhenPosition = Math.floor(Math.random()* when.length);
+  let randomactionsPosition = Math.floor(Math.random()* actions.length);
+  
+  let randomWho= who[randomWhoPosition];
+  let randomWhat= who[randomWhatPosition];
+  let randomWhen= who[randomWhenPosition];
+  let randomActions= who[randomactionsPosition];
+  
+  document.getElementById("excuse").innerHTML = randomWho + " " + randomWhat + " " + randomActions + " " + randomWhen + " " ;
 };
-let randomWhoPosition = Math.floor(Math.random()* who.length);
